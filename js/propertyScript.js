da7b510263c44fb62ec95fac25cc7692ce528251
thamudhifernando <fernandothamudhi@gmail.com>
2021-01-03 01:57:02 +0530

    add detail tabs

---
+++ js/propertyScript.js
@@ -0,0 +1,10 @@
+function redirectTab(idValue,elmnt) {
+    var i, tabPaneContent, tabBtn;
+    tabPaneContent = document.getElementsByClassName("tab-pane-content");
+    for (i = 0; i < tabPaneContent.length; i++) {
+        tabPaneContent[i].style.display = "none";
+    }
+    document.getElementById(idValue).style.display = "block";
+}
+
+document.getElementById("defaultTab").click();
