2967c841c4c8019e606b012da0cf288f49f30365
thamudhifernando <fernandothamudhi@gmail.com>
2021-01-05 13:42:35 +0530

    add 5 jQuery UI widgets on form

--- js/searchPageScript.js
+++ js/searchPageScript.js
@@ -1,4 +1,38 @@
 // Form Script
+
+// Property Type Select Widget Function
+$(function () {
+    $("#propertyType").selectmenu();
+});
+
+// Number of bedrooms Spinner Widget Function
+$(function () {
+    $("#mini-bedrooms").spinner();
+    $("#max-bedrooms").spinner();
+    if ( $("#mini-bedrooms").spinner( "instance" ) &&  $("#max-bedrooms").spinner( "instance" )  ) {
+        $("#mini-bedrooms").spinner( "destroy" );
+        $("#max-bedrooms").spinner( "destroy" );
+    } else {
+        $("#mini-bedrooms").spinner();
+        $("#max-bedrooms").spinner();
+    }
+});
+
+// Price range Slider Widget Function
+$(function () {
+    $("#price-range").slider({
+        range: true,
+        min: 10000,
+        max: 800000,
+        values: [50, 200],
+        slide: function (event, ui) {
+            $("#price-text").text("Rs." + ui.values[0] + " - Rs." + ui.values[1]);
+        }
+    });
+    $("#price-text").val("Rs."+ $("#price-range").slider("values",0) +" - Rs."+ $("#price-range").slider("values",1) );
+});
+
+
 // Submit Button Click Function
 $(function () {
     $(".widget form button").button();
@@ -14,30 +48,26 @@ $(function () {
 
     var dateFormat = "dd/mm/yyyy";
 
-    var startDate = $("#start-added-date").datepicker(
-        {
-            changeMonth: true,
-            changeYear:true,
-            numberOfMonths: 2,
-            duration: "normal",
-            prevText:"click for previous months",
-            nextText:"click for next months",
-            maxDate: "defaultDate"
-        }
-    ).on("change", function () {
+    var startDate = $("#start-added-date").datepicker({
+        changeMonth: true,
+        changeYear: true,
+        numberOfMonths: 2,
+        duration: "normal",
+        prevText: "click for previous months",
+        nextText: "click for next months",
+        maxDate: "defaultDate"
+    }).on("change", function () {
         endDate.datepicker("option", "startDate", getAddedDate(this))
     });
-    var endDate = $("#end-added-date").datepicker(
-        {
-            changeMonth: true,
-            changeYear:true,
-            numberOfMonths: 2,
-            duration: "normal",
-            prevText:"click for previous months",
-            nextText:"click for next months",
-            maxDate: "defaultDate"
-        }
-    ).on("change", function () {
+    var endDate = $("#end-added-date").datepicker({
+        changeMonth: true,
+        changeYear: true,
+        numberOfMonths: 2,
+        duration: "normal",
+        prevText: "click for previous months",
+        nextText: "click for next months",
+        maxDate: "defaultDate"
+    }).on("change", function () {
         startDate.datepicker("option", "endDate", getAddedDate(this))
     });
 
